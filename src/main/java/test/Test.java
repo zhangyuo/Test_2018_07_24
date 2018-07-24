705696f6862ce6da5435c3dc6f59c64b0c540d99
zhangyu <zhangyuo@zbj.com>
2018-07-24 15:35:17 +0800

    initilize project

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author zhangyuo@zbj.com
+ * @date 2018/7/24 15:31
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Welcome to github!");
+    }
+}
